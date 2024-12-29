f8515782c34e96f628c8e70302445bec9b6a557d
ergashelmonov <elmonovergash@gmail.com>
2024-12-29 18:06:46 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Elmed-clinics/", // Bu yerda repository nomini ko‘rsating
+  build: {
+    outDir: "build", // Agar build papkasi boshqacha bo'lsa
+  },
+});
+  
